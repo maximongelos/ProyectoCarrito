076ce997049cdde8f1f2ae0804faa96346fcb929
Maximiliano <maximongelos@gmail.com>
2022-07-12 04:18:13 -0300

    Cambio funcionalidad de itemCount e itemDetail

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -1,38 +1,26 @@
 import React from 'react';
 import {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
 
-const ItemCount = ({initialStock}) => {
-	const [count, setCount] = useState(1);
-	const [stock, setStock] = useState(initialStock);
-
-	const navigate = useNavigate();
+const ItemCount = ({initial, initialStock, onConfirm}) => {
+	const [count, setCount] = useState(initial);
 
 	const addProduct = (num) => {
 		setCount(count + num);
 	};
 
-	const handleAdd = (e) => {
-		e.preventDefault();
-		if (stock > 0) {
-			setStock(stock - count);
-			setCount(1);
+	const handleConfirm = () => {
+		if (count <= initialStock) {
+			onConfirm(count);
+		} else {
+			alert('No puede superar el stock');
 		}
-
-		alert(`Agregaste ${count} productos al carrito`);
 	};
-
-	const handleTerminate = () => {
-		navigate('/cart');
-	};
-
-	console.log(`Count: ${count} - Stock: ${stock}`);
 
 	return (
 		<>
 			<div className="flex justify-center text-center text-sm border-2 w-1/2 rounded-md p-2 cursor-pointer tablet:w-3/5 cel:w-3/5">
 				<button
-					disabled={count === 1 ? true : null}
+					disabled={count === initial ? true : null}
 					onClick={() => addProduct(-1)}
 					className="text-sm font-bold pr-7"
 				>
@@ -40,7 +28,7 @@ const ItemCount = ({initialStock}) => {
 				</button>
 				{count}
 				<button
-					disabled={count === stock || stock === 0 ? true : null}
+					disabled={count === initialStock || initialStock === 0 ? true : null}
 					onClick={() => addProduct(1)}
 					className="text-sm font-bold pl-7"
 				>
@@ -48,23 +36,13 @@ const ItemCount = ({initialStock}) => {
 				</button>
 			</div>
 			<br />
-			{stock === initialStock ? (
-				<button
-					disabled={stock === 0 ? true : null}
-					className="flex justify-center text-center text-sm border-2 w-1/2 rounded-md p-2 cursor-pointer uppercase hover:bg-gris tablet:w-3/5 cel:w-3/5"
-					onClick={handleAdd}
-				>
-					Agregar al carrito
-				</button>
-			) : (
-				<button
-					disabled={stock === 0 ? true : null}
-					className="flex justify-center text-center text-sm border-2 w-1/2 rounded-md p-2 cursor-pointer uppercase hover:bg-gris tablet:w-3/5 cel:w-3/5"
-					onClick={handleTerminate}
-				>
-					Finalizar compra
-				</button>
-			)}
+			<button
+				disabled={initialStock === 0 ? true : null}
+				className="flex justify-center text-center text-sm border-2 w-1/2 rounded-md p-2 cursor-pointer uppercase hover:bg-gris tablet:w-3/5 cel:w-3/5"
+				onClick={handleConfirm}
+			>
+				Agregar al carrito
+			</button>
 		</>
 	);
 };
--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import ItemCount from '../ItemCount';
+import {useNavigate} from 'react-router-dom';
+import {useState} from 'react';
 
 const ItemDetail = ({product}) => {
+	const navigate = useNavigate();
+
+	product.stock = 10;
+
+	const [qtyAdded, setQtyAdded] = useState(0);
+
+	const handleConfirm = (qty) => {
+		setQtyAdded(qty);
+	};
+
+	const handleTerminate = () => {
+		navigate('/cart');
+	};
+
 	return (
 		<div className="flex justify-end items-end  w-4/5 mx-0 my-auto pt-6 tablet:flex-col tablet:justify-center tablet:items-center tablet:w-full cel:flex-col cel:justify-center cel:items-center cel:w-full">
 			<div className=" w-2/5 items-center p-6">
@@ -22,7 +38,20 @@ const ItemDetail = ({product}) => {
 				<p className="w-10/12 text-base text-justify pb-3 tablet:text-center cel:text-center">
 					{product.description}
 				</p>
-				<ItemCount initialStock={10} />
+				{qtyAdded === 0 ? (
+					<ItemCount
+						initialStock={product.stock}
+						initial={1}
+						onConfirm={handleConfirm}
+					/>
+				) : (
+					<button
+						className="flex justify-center text-center text-sm border-2 w-1/2 rounded-md p-2 cursor-pointer uppercase hover:bg-gris tablet:w-3/5 cel:w-3/5"
+						onClick={handleTerminate}
+					>
+						Finalizar compra
+					</button>
+				)}
 			</div>
 		</div>
 	);
